043f84e480a74c4ab240926dcfba0c6cf1f829dd
seydou ba <seydouba03@gmail.com>
2020-11-08 14:05:46 +0000

    feat page know us

---
+++ src/app/components/knowus/knowus.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './knowus.component.html',
+  styleUrls: ['./knowus.component.css']
+})
+export class KnowusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
